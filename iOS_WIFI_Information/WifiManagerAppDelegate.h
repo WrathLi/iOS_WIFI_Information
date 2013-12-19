83bd07bc3875931936be2c7485e88a75a8e4d459
WrathLi <liyux4869@126.com>
2013-12-19 16:52:40 +0800

    Initial Commit

---
+++ iOS_WIFI_Information/WifiManagerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WifiManagerAppDelegate.h
+//  iOS_WIFI_Information
+//
+//  Created by leo on 13-12-19.
+//  Copyright (c) 2013年 leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WifiManagerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS_WIFI_Information/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS_WIFI_Information
+//
+//  Created by leo on 13-12-19.
+//  Copyright (c) 2013年 leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WifiManagerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([WifiManagerAppDelegate class]));
+	}
+}
